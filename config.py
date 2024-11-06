c0d826936300259727ed07604d0b494dfcdc8a86
IDGS-803-21000029 <21000029@alumnos.utleon.edu.mx>
2024-11-06 00:20:35 -0600

    update settings

--- config.py
+++ config.py
@@ -1,4 +1,22 @@
-# config.py
-#DATABASE_URL = "postgresql://openpg:openpgpwd@localhost:5432/bazarcontext"
-DATABASE_URL = "postgresql://bazzarcontext_user:YMVJuHTOeuwyTGvYyK4VJsTDBB4XAgNe@dpg-cslfti3qf0us738sj1hg-a.oregon-postgres.render.com/bazzarcontext"
+import os
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Obtén la clave de cifrado y la URL cifrada desde las variables de entorno
+ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
+ENCRYPTED_DATABASE_URL = os.getenv("ENCRYPTED_DATABASE_URL")
+\
+print("Clave de cifrado:", ENCRYPTION_KEY)
+print("URL cifrada:", ENCRYPTED_DATABASE_URL)
+
+# Inicializa el objeto de cifrado
+cipher_suite = Fernet(ENCRYPTION_KEY)
+
+def get_database_url():
+    # Desencripta la `DATABASE_URL`
+    decrypted_url = cipher_suite.decrypt(ENCRYPTED_DATABASE_URL.encode()).decode()
+    return decrypted_url
+
+DATABASE_URL = get_database_url()
